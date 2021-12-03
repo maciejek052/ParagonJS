136051d0b2e076c913538e07d087de2db23fa16d
Maciej Czech <77523@student.pb.edu.pl>
2021-12-03 20:33:09 +0100

    dodawanie produktów

--- index.js
+++ index.js
@@ -1,65 +1,56 @@
 // wyswietlanie naglowka z data
 let naglowek = document.createElement('h2');
-naglowek.style.textAlign = "center"; 
-var d = new Date(); 
-var aktualnaData = d.getDate()  + "-" + (d.getMonth()+1) + "-" + d.getFullYear() + " " + d.getHours() + ":" + String(d.getMinutes()).padStart(2, "0");;
-naglowek.textContent = "Paragon z dnia " + aktualnaData; 
+naglowek.style.textAlign = "center";
+var d = new Date();
+var aktualnaData = d.getDate() + "-" + (d.getMonth() + 1) + "-" + d.getFullYear() + " " + d.getHours() + ":" + String(d.getMinutes()).padStart(2, "0");;
+naglowek.textContent = "Paragon z dnia " + aktualnaData;
 document.body.prepend(naglowek);
- 
-let listaProduktow=[]
-
-const tablica=document.getElementById("tabela")
-var id; 
-
-const dodajWiersz=(id, produkt)=>
-{
-let wiersz=tablica.insertRow()
-row.class="produkt"
-let index=wiersz.insertCell(0)
-let nazwa=wiersz.insertCell(1)
-let ilosc=wiersz.insertCell(2)
-let suma=wiersz.insertCell(3)
-
-
-index.id="id"
-nazwa.innerHTML=produkt.nazwa
-ilosc.innerHTML=produkt.ilosc
-suma.innerHTML=Math.round(produkt.ilosc*produkt.cena)
-}
-
 
 class Produkt {
-  constructor(nazwa,ilosc,cena) {
+  constructor(nazwa, ilosc, cena) {
     this.nazwa = nazwa;
     this.ilosc = ilosc;
     this.cena = cena;
+    this.suma = (ilosc * cena);
+  };
+};
+
+let listaProduktow = []; 
+const tabela = document.getElementById("tabela");
+document.body.onload = wczytajLocalStorage();
+
+// funkcja odpowiedzialna za dodanie produktu do listy
+const formularz = document.getElementById("formularz");
+function nowyProdukt() {
+  let prod = new Produkt(formularz.nazwa.value, formularz.ilosc.value, formularz.cena.value);
+  listaProduktow.push(prod);
+  dodajDoTabeli(listaProduktow.length - 1, prod);
+  localStorage.paragon = JSON.stringify(listaProduktow);
+}
+
+// funkcja odpowiedzialna za wyświetlenie produktu w tabeli na stronie
+function dodajDoTabeli(id, produkt) {
+  let wiersz = tabela.insertRow();
+  let lp = wiersz.insertCell(0);
+  let nazwa = wiersz.insertCell(1);
+  let ilosc = wiersz.insertCell(2);
+  let cena = wiersz.insertCell(3);
+  let suma = wiersz.insertCell(4);
+  let usun = wiersz.insertCell(5);
+  let edytuj = wiersz.insertCell(6);
+  lp.innerHTML = id + 1;
+  nazwa.innerHTML = produkt.nazwa;
+  ilosc.innerHTML = produkt.ilosc;
+  cena.innerHTML = produkt.cena;
+  suma.innerHTML = produkt.suma;
+  usun.innerHTML = "<button class='btn btn-danger'>Usuń</button>";
+  edytuj.innerHTML = "<button class='btn btn-warning'>Edytuj</button>";
+};
+
+// funkcja odpowiedzialna za wczytanie produktów z local storage
+function wczytajLocalStorage() {
+  if (localStorage.paragon) {
+    listaProduktow = JSON.parse(localStorage.paragon);
+    listaProduktow.forEach((produkt, id) => dodajDoTabeli(id, produkt));
   }
-  suma() {
-    return this.ilosc * this.cena;
-  }
 }
-class Paragon{
-    constructor()
-    {
-        this.tab=[]
-    }
-
-
-}
-dodajProdukt( produkt)
-{
-    const 
-    this.tab.push(produkt)
-
-}
-usunProdukt(id)
-{
-    this.array.slice(id, 1)
-}
-edytujProdukt(produkt)
-{
-    
-}
-
-
-let ar = []; 
